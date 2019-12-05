95d34834fecda00c0fce6727a98d58200600dc4c
Lukasz Kurek <lukascoorek@gmail.com>
2019-12-05 22:38:59 +0100

    Updates

---
+++ precache-manifest.e869813a49d1e1588be1368b170162dc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b906432f7c7e910bdccec3d8dadc496",
+    "url": "/starwars-app/index.html"
+  },
+  {
+    "revision": "7e1b2516c6af87559805",
+    "url": "/starwars-app/static/css/main.aa604fef.chunk.css"
+  },
+  {
+    "revision": "4543dfa0427d3cf7434a",
+    "url": "/starwars-app/static/js/2.05720f49.chunk.js"
+  },
+  {
+    "revision": "7e1b2516c6af87559805",
+    "url": "/starwars-app/static/js/main.fe9832e9.chunk.js"
+  },
+  {
+    "revision": "e59135bf2d8b288ef9c5",
+    "url": "/starwars-app/static/js/runtime-main.d4781398.js"
+  },
+  {
+    "revision": "c41f740a5bf4b1e663ee3184b08677f7",
+    "url": "/starwars-app/static/media/Star_Wars_logo.c41f740a.svg"
+  },
+  {
+    "revision": "f72f509a2014f09c1ebf1c81294ed792",
+    "url": "/starwars-app/static/media/Starjout.f72f509a.ttf"
+  },
+  {
+    "revision": "eb4e10bc9c839e7561b6da7e2b962cfb",
+    "url": "/starwars-app/static/media/falcon-1024.eb4e10bc.jpg"
+  },
+  {
+    "revision": "4c2f808e36c4e3213e796055bd3dc3a2",
+    "url": "/starwars-app/static/media/falcon-1920.4c2f808e.jpg"
+  },
+  {
+    "revision": "859de365af8c225dc3abf123f579e17d",
+    "url": "/starwars-app/static/media/falcon-404.859de365.jpeg"
+  },
+  {
+    "revision": "50e2f42abcbe89b72f5b0dd352282471",
+    "url": "/starwars-app/static/media/falcon.50e2f42a.png"
+  },
+  {
+    "revision": "c55d8bed23354f6c16e36981cda8999b",
+    "url": "/starwars-app/static/media/lightsaber.c55d8bed.png"
+  }
+]);
