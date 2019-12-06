a0e58427f61e396232eef406fa614b22f6b93286
Lukasz Kurek <lukascoorek@gmail.com>
2019-12-06 13:16:23 +0100

    add animation arrow to hero component

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Section from "./Section";
 
 const Hero = () => {
+  const [iClass, setiClass] = useState(false);
+  useEffect(() => {
+    const handleScroll = e => {
+      if (e.srcElement.scrollingElement.scrollTop > 10) {
+        setiClass(true);
+      } else {
+        setiClass(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [iClass]);
+
   return (
     <>
       <section className="hero">
         <h1>
           <span>power</span> <span>source</span>
         </h1>
+        <i className={`hero-arrow ${iClass ? "hero-arrow__hide" : ""}`}></i>
       </section>
       <Section />
     </>
